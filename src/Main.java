b300fd05671a3f516b6c4c520034f12c433777bc
gigyesik <gigyesik@gmail.com>
2024-01-28 15:54:30 +0900

    Fundamentals - Loop

--- src/Main.java
+++ src/Main.java
@@ -106,6 +106,35 @@ public class Main extends ab {
 
         // abstract method
         m.abMethod();
+
+        // Loop
+        // for statement
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        // for-each statement
+        int arr[] = {12, 34, 56, 78};
+
+        for (int num : arr) {
+            System.out.println(num);
+        }
+
+        // while statement
+        int i3 = 1;
+
+        while (i3 <= 10) {
+            System.out.println(i3);
+            i3++;
+        }
+
+        // do-while statement
+        int i4 = 1;
+
+        do {
+            System.out.println(i4);
+            i4++;
+        } while (i4 <= 10);
     }
 
     // instance method
