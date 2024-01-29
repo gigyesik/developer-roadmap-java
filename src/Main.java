6242b70292138992973a7a95b25d322cbc5cb912
gigyesik <gigyesik@gmail.com>
2024-01-30 02:41:33 +0900

    Fundamentals - Exception Handling

--- src/Main.java
+++ src/Main.java
@@ -135,6 +135,33 @@ public class Main extends ab {
             System.out.println(i4);
             i4++;
         } while (i4 <= 10);
+
+        // Exception Handling
+        // printStackTrace()
+        int a = 5;
+        int b = 0;
+
+//        try {
+//            System.out.println(a / b);
+//        } catch (ArithmeticException e) {
+//            e.printStackTrace();
+//            System.out.println(e.toString());
+//            System.out.println(e.getMessage());
+//        }
+
+        // default Exception handler
+//        String str = null;
+//        System.out.println(str.length());
+
+        // try catch finally
+//        try {
+//            int[] arr2 = new int[4];
+//            int i = arr2[4];
+//        } catch (Exception e) {
+//            throw e;
+//        } finally {
+//            System.out.println("Need to execute");
+//        }
     }
 
     // instance method
