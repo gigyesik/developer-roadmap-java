36026f7d9e5dd769ffcb64a3e750771968c16730
gigyesik <gigyesik@gmail.com>
2024-02-02 00:39:04 +0900

    Fundamentals - Package 6

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Exception Handling
-        // printStackTrace()
-        int a = 5;
-        int b = 0;
 
-//        try {
-//            System.out.println(a / b);
-//        } catch (ArithmeticException e) {
-//            e.printStackTrace();
-//            System.out.println(e.toString());
-//            System.out.println(e.getMessage());
-//        }
-
-        // default Exception handler
-//        String str = null;
-//        System.out.println(str.length());
-
-        // try catch finally
-//        try {
-//            int[] arr2 = new int[4];
-//            int i = arr2[4];
-//        } catch (Exception e) {
-//            throw e;
-//        } finally {
-//            System.out.println("Need to execute");
-//        }
     }
 
 
---
+++ src/fundamental/ExceptionHanding.java
@@ -0,0 +1,32 @@
+package fundamental;
+
+public class ExceptionHanding {
+    public static void main(String[] args) {
+        // Exception Handling
+        // printStackTrace()
+        int a = 5;
+        int b = 0;
+
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            System.out.println(e.toString());
+            System.out.println(e.getMessage());
+        }
+
+        // default Exception handler
+        String str = null;
+        System.out.println(str.length());
+
+        // try catch finally
+        try {
+            int[] arr2 = new int[4];
+            int i = arr2[4];
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            System.out.println("Need to execute");
+        }
+    }
+}
