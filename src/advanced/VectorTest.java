57d65255af87f6b3a8645c735003cc825ba575e9
gigyesik <gigyesik@gmail.com>
2024-02-14 03:12:48 +0900

    Advanced - Vector

---
+++ src/advanced/VectorTest.java
@@ -0,0 +1,19 @@
+package advanced;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class VectorTest {
+    public static void main(String[] args) {
+        Vector<String > stringVector = new Vector<>();
+        stringVector.add("a");
+        stringVector.add("b");
+        stringVector.add("c");
+        stringVector.add("d");
+
+        Iterator<String> iterator = stringVector.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next()); // a b c d
+        }
+    }
+}
