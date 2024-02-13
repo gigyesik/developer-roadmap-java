c325ca3844be5426ee0efd7137f852b8ff42de91
gigyesik <gigyesik@gmail.com>
2024-02-14 01:01:10 +0900

    Advanced - List

---
+++ src/advanced/ListTest.java
@@ -0,0 +1,10 @@
+package advanced;
+
+import java.util.*;
+
+public class ListTest {
+    List<String> stringList = new ArrayList<>();
+    List<Long> longList = new LinkedList<>();
+    List<Integer> integerList = new Vector<>();
+    List<Float> floatList = new Stack<>();
+}
