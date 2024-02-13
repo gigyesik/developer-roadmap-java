e7319ce481d7a8f94dceca2349fdcb0ad65173ac
gigyesik <gigyesik@gmail.com>
2024-02-14 01:15:04 +0900

    Advanced - LinkedList

---
+++ src/advanced/LinkedListTest.java
@@ -0,0 +1,20 @@
+package advanced;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList<Long> longLinkedList = new LinkedList<>();
+        longLinkedList.add(1L);
+        longLinkedList.add(2L);
+        longLinkedList.add(3L);
+        longLinkedList.add(4L);
+
+        Iterator<Long> iterator = longLinkedList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
