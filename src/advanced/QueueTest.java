80e2f6d6bf4dc8b734578866738dc4d8ec5737b5
gigyesik <gigyesik@gmail.com>
2024-02-17 17:06:12 +0900

    Advanced - Queue

---
+++ src/advanced/QueueTest.java
@@ -0,0 +1,12 @@
+package advanced;
+
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueTest {
+    public static void main(String[] args) {
+        Queue<String> q1 = new PriorityQueue<>();
+        Queue<Integer> q2 = new ArrayDeque<>();
+    }
+}
