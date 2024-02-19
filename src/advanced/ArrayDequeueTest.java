42aa7b6d139c1c2e06393b3d89f21a2e5b5606ad
gigyesik <gigyesik@gmail.com>
2024-02-20 03:30:47 +0900

    Advanced - ArrayDequeue

---
+++ src/advanced/ArrayDequeueTest.java
@@ -0,0 +1,19 @@
+package advanced;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ArrayDequeueTest {
+    public static void main(String[] args) {
+        Deque<String> deque = new ArrayDeque<>();
+
+        deque.add("a");
+        deque.add("b");
+        deque.add("c");
+
+        for (String s : deque) {
+            System.out.println(s); // a b c
+        }
+    }
+
+}
