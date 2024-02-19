c154feb50c6b71e7660a1dfb38caf0d707607568
gigyesik <gigyesik@gmail.com>
2024-02-20 03:35:36 +0900

    Advanced - HashSet

---
+++ src/advanced/HashSetTest.java
@@ -0,0 +1,20 @@
+package advanced;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetTest {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("a");
+        set.add("b");
+        set.add("c");
+        set.add("d");
+
+        Iterator<String> iterator = set.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next()); // a b c d
+        }
+    }
+}
