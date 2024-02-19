4e24c8d75e74a7b4f4217c343915a6182b38e4ff
gigyesik <gigyesik@gmail.com>
2024-02-20 03:37:20 +0900

    Advanced - LinkedHashSet

---
+++ src/advanced/LinkedHashSetTest.java
@@ -0,0 +1,20 @@
+package advanced;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetTest {
+    public static void main(String[] args) {
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+        set.add("a");
+        set.add("b");
+        set.add("c");
+        set.add("d");
+
+        Iterator<String> iterator = set.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next()); // a b c d
+        }
+    }
+}
