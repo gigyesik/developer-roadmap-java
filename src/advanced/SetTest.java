e586b9b890a9dc2edfa745b6a56737013957b831
gigyesik <gigyesik@gmail.com>
2024-02-20 03:32:04 +0900

    Advanced - Set

---
+++ src/advanced/SetTest.java
@@ -0,0 +1,12 @@
+package advanced;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetTest {
+    Set<String> s1 = new HashSet<>();
+    Set<Integer> s2 = new LinkedHashSet<>();
+    Set<Long> s3 = new TreeSet<>();
+}
