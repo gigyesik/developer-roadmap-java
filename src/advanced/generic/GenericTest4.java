7f9e1010bed628837cfae2da516cff2cb2b74c8d
gigyesik <gigyesik@gmail.com>
2024-02-27 03:37:06 +0900

    Advanced - Generic can arrayList

--- src/advanced/generic/GenericTest4.java
+++ src/advanced/generic/GenericTest4.java
@@ -1,5 +1,7 @@
 package advanced.generic;
 
+import java.util.ArrayList;
+
 public class GenericTest4<T> {
     T obj;
     GenericTest4(T obj) { this.obj = obj; }
@@ -10,6 +12,7 @@ public class GenericTest4<T> {
         System.out.println(intObj.getObj()); // 10
 
         // GenericTest4<int> iObj = new GenericTest4<int>(22); // primitive type 불가
+        GenericTest4<ArrayList<int[]>> aiObj = new GenericTest4<>(new ArrayList<>()); // ArrayList 가 이미 referenced type 이므로 가능
 
         GenericTest4<String> stObj = new GenericTest4<>("ABC");
         System.out.println(stObj.getObj()); // ABC
