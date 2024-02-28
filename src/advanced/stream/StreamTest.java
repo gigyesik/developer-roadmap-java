d2db9847fd3a04e0750fc3778fd29e42eb6a3913
gigyesik <gigyesik@gmail.com>
2024-02-29 02:39:42 +0900

    Advanced - Stream Filter

--- src/advanced/stream/StreamTest.java
+++ src/advanced/stream/StreamTest.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 public class StreamTest {
     List<Integer> integerList = List.of(1,2,3,4,5);
-
-
+    // map()
     List<Integer> streamIntList = integerList.stream().map(x -> x*x).toList();
+
+    List<String> stringList = List.of("Reflection", "Collection", "Stream");
+    // filter()
+    List<String> streamStringList = stringList.stream().filter(s -> s.startsWith("S")).toList();
+
     public static void main(String[] args) {
         StreamTest streamTest = new StreamTest();
         System.out.println(streamTest.streamIntList); // 1 4 9 16 25
+        System.out.println(streamTest.streamStringList); // Stream
     }
 }
