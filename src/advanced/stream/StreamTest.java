f9b106601e16b5c4a91c13f1d0808b85001721da
gigyesik <gigyesik@gmail.com>
2024-02-29 02:48:15 +0900

    Advanced - Stream Collect

--- src/advanced/stream/StreamTest.java
+++ src/advanced/stream/StreamTest.java
@@ -1,6 +1,8 @@
 package advanced.stream;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class StreamTest {
     List<Integer> integerList = List.of(1,2,3,4,5);
@@ -14,10 +16,15 @@ public class StreamTest {
     // sorted()
     List<String> sortedStringList = stringList.stream().sorted().toList();
 
+    List<Integer> integerList2 = List.of(2,4,2,5,4);
+    // collect()
+    Set<Integer> streamIntSet = integerList2.stream().collect(Collectors.toSet());
+
     public static void main(String[] args) {
         StreamTest streamTest = new StreamTest();
         System.out.println(streamTest.streamIntList); // [1, 4, 9, 16, 25]
         System.out.println(streamTest.streamStringList); // [Stream]
         System.out.println(streamTest.sortedStringList); // [Collection, Reflection, Stream]
+        System.out.println(streamTest.streamIntSet); // 2, 4, 5
     }
 }
