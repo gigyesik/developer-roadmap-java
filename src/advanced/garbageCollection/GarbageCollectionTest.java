7e9e1bc129c75f18f56c3dc6137c1df7669349a7
gigyesik <gigyesik@gmail.com>
2024-02-29 15:43:34 +0900

    Advanced - Garbage Collection 2

--- src/advanced/garbageCollection/GarbageCollectionTest.java
+++ src/advanced/garbageCollection/GarbageCollectionTest.java
@@ -6,5 +6,13 @@ public class GarbageCollectionTest {
         GarbageCollectionTest g1 = new GarbageCollectionTest();
         g1 =  null;
         System.out.println(g1); // null
+
+        // Assigning Reference to Another
+        GarbageCollectionTest g2 = new GarbageCollectionTest();
+        GarbageCollectionTest g3 = new GarbageCollectionTest();
+        System.out.println(g2);
+        System.out.println(g3);
+        g2 = g3;
+        System.out.println(g2);
     }
 }
