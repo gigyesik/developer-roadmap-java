7dc7d2173fd5d4e1fe7c9ae7466b270ff82fb460
gigyesik <gigyesik@gmail.com>
2024-02-29 15:32:56 +0900

    Advanced - Garbage Collection 1

---
+++ src/advanced/garbageCollection/GarbageCollectionTest.java
@@ -0,0 +1,10 @@
+package advanced.garbageCollection;
+
+public class GarbageCollectionTest {
+    public static void main(String[] args) {
+        // Nulling Object
+        GarbageCollectionTest g1 = new GarbageCollectionTest();
+        g1 =  null;
+        System.out.println(g1); // null
+    }
+}
